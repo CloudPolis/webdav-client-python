3d3fd2a4a3343dc45867abc022f41ed0b7ec08d0
designerror <designerror@yandex.ru>
2014-10-24 23:57:38 +0400

    Updating readme

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name     = 'webdavclient',
-    version  = '0.1.14',
+    version  = '0.1.15',
     packages = find_packages(),
     requires = ['python (>= 2.7.6)'],
-    description  = 'Webdav API, resource API and webdav tool for Webdav services (Yandex.Disk, DropBox, GoogleDrive, Box, 4shared)',
+    description  = 'Webdav API, resource API and webdav tool for WebDAV services (Яндекс.Диск, Dropbox, Google Диск, Box и 4shared)',
     long_description = open('README.rst').read(),
+    author = 'Designerror',
     author_email = 'designerror@yandex.ru',
     url          = 'https://github.com/designerror/webdavclient',
     download_url = 'https://github.com/designerror/webdavclient/tarball/master',
