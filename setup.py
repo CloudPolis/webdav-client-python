7e0b44a9c5bb935bba0413dc9ef8ac86ffea842b
designerror <designerror@yandex.ru>
2014-10-24 20:09:35 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ from setuptools import setup, find_packages
 
 setup(
     name     = 'webdavclient',
-    version  = '0.1.8',
+    version  = '0.1.9',
     packages = find_packages(),
     requires = ['python (>= 2.7.6)'],
     description  = 'Webdav API, resource API and webdav tool for Webdav services (Yandex.Disk)',
-    long_description = open('README.rst').read(), 
-    author       = 'Oleg Postnikov',
+    long_description = open('README.rst').read(),
     author_email = 'designerror@yandex.ru',
     url          = 'https://github.com/designerror/webdavclient',
     download_url = 'https://github.com/designerror/webdavclient/tarball/master',
