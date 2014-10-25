3bf3dc3a350baed4f53a4a986e6b279b07714afa
designerror <designerror@yandex.ru>
2014-10-25 12:47:40 +0400

    adding check connection to wdc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name     = 'webdavclient',
-    version  = '0.2.0',
+    version  = '0.2.1',
     packages = find_packages(),
     requires = ['python (>= 2.7.6)'],
     install_requires=['pycurl'],
