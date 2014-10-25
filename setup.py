e4f853843eb9a9b0dc5a37b69fd4fc0398d2d805
designerror <designerror@yandex.ru>
2014-10-25 12:13:04 +0400

    Updating readme

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages = find_packages(),
     requires = ['python (>= 2.7.6)'],
     install_requires=['pycurl'],
-    scripts = ['webdavtool'],
+    scripts = ['wdc'],
     description  = 'Webdav API, resource API and webdav tool for WebDAV services (Яндекс.Диск, Dropbox, Google Диск, Box и 4shared)',
     long_description = open('README.rst').read(),
     author = 'Designerror',
