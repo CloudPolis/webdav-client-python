baefa20ab2efd6aa615bd3a54e7023a2369671d7
designerror <designerror@yandex.ru>
2014-10-25 12:03:40 +0400

    Adding installing webdavtool

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name     = 'webdavclient',
-    version  = '0.1.17',
+    name     = 'Webdavclient',
+    version  = '0.1.18',
     packages = find_packages(),
     requires = ['python (>= 2.7.6)'],
     install_requires=['pycurl'],
+    scripts = ['webdavtool'],
     description  = 'Webdav API, resource API and webdav tool for WebDAV services (Яндекс.Диск, Dropbox, Google Диск, Box и 4shared)',
     long_description = open('README.rst').read(),
     author = 'Designerror',
