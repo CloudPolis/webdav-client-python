b62675a04a939a81a2cd5733f928058a2dfb1383
designerror <designerror@yandex.ru>
2014-12-29 16:06:32 +0300

    Enabled method free

--- webdav/client.py
+++ webdav/client.py
@@ -122,9 +122,11 @@ class Client(object):
         server_token = '{login}:{password}'.format(login=self.webdav.login, password=self.webdav.password)
         self.default_options.update({
             'URL': self.webdav.hostname,
+            'SSL_VERIFYPEER': 0,
+            'SSL_VERIFYHOST': 0,
             'USERPWD': server_token,
-            'SSLVERSION': pycurl.SSLVERSION_TLSv1,
             'NOBODY': 1
+            #'SSLVERSION': pycurl.SSLVERSION_TLSv1,
         })
 
         if self.proxy.valid():
