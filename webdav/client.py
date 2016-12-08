230b9ebf4227d8872073249eb202411a6df9b6e5
designerror <designerror@yandex.ru>
2016-12-08 09:39:28 +0300

    Update client.py

--- webdav/client.py
+++ webdav/client.py
@@ -163,6 +163,9 @@ class Client(object):
 
         if self.webdav.key_path:
             self.default_options['SSLKEY'] = self.webdav.key_path
+            
+        self.default_options['SSL_VERIFYHOST'] = 0
+        self.default_options['SSL_VERIFYPEER'] = 0
 
         if self.webdav.recv_speed:
             self.default_options['MAX_RECV_SPEED_LARGE'] = self.webdav.recv_speed
